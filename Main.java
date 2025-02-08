13d029ce8a03ef590c0d21f95d8a1aea2bbeb044
Davi Romauski Meurer <daviromauski@gmail.com>
2025-02-07 22:49:18 -0300

    Atualização

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-// Classe que representa o tipo de árvore (estado compartilhado)
+
 class TipoArvore {
     private final String nome;
     private final String cor;
@@ -20,7 +20,7 @@ class TipoArvore {
     }
 }
 
-// Fábrica que gerencia instâncias de TipoArvore (Flyweight Factory)
+
 class TipoArvoreFactory {
     private static final Map<String, TipoArvore> tipos = new HashMap<>();
 
@@ -31,7 +31,7 @@ class TipoArvoreFactory {
     }
 }
 
-// Classe que representa uma árvore específica (estado extrínseco)
+
 class Arvore {
     private final int x, y;
     private final TipoArvore tipo;
@@ -47,19 +47,19 @@ class Arvore {
     }
 }
 
-// Classe principal para testar o Flyweight
+
 public class Main {
     public static void main(String[] args) {
         List<Arvore> floresta = new ArrayList<>();
         
-        // Criando árvores reutilizando instâncias compartilhadas
+        
         floresta.add(new Arvore(10, 20, TipoArvoreFactory.getTipoArvore("Carvalho", "Verde", "Rugosa")));
         floresta.add(new Arvore(15, 25, TipoArvoreFactory.getTipoArvore("Carvalho", "Verde", "Rugosa")));
         floresta.add(new Arvore(30, 40, TipoArvoreFactory.getTipoArvore("Pinheiro", "Verde Escuro", "Lisa")));
         floresta.add(new Arvore(50, 60, TipoArvoreFactory.getTipoArvore("Pinheiro", "Verde Escuro", "Lisa")));
         floresta.add(new Arvore(70, 80, TipoArvoreFactory.getTipoArvore("Carvalho", "Verde", "Rugosa")));
         
-        // Exibir todas as árvores na floresta
+        
         for (Arvore arvore : floresta) {
             arvore.exibir();
         }
